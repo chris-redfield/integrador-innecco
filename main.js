d8ec98f14aa75d82897c828768affc6f09568d0e
Christian Moryah <christianmoryah@gmail.com>
2016-01-23 20:51:24 -0200

    checando se o arquivo existe antes de salvar

--- main.js
+++ main.js
@@ -8,7 +8,10 @@ var http = require("http");
 //lib para io com o file system
 var fs = require("fs");
 
-var porta = "8080";
+var porta = "8888";
+
+//TO-DO: Especificar decentemente qual será o caminho do arquivo
+var pastaJsonVend = "./jsonVend/";
 
 // Processa um request do tipo POST
 function processPost(request, response, callback) {
@@ -36,17 +39,34 @@ function processPost(request, response, callback) {
         //jsontiza os dados do request
         var jsonVend = JSON.parse(queryData);
 
-        //compoe o nome do arquivo com data+T+hora+cpf.json
+        //compoe o nome do arquivo com data+T+hora+cpf
         nomeArquivo = jsonVend.sale_date.replace(" ", "T") + "-";
-        nomeArquivo += jsonVend.customer.customer_code + ".json";
-        console.log(nomeArquivo);
+        nomeArquivo += jsonVend.customer.customer_code;
 
-        //TO-DO: Especificar decentemente qual será o caminho do arquivo
-        fs.writeFile( "./jsonVend/"+nomeArquivo, JSON.stringify(jsonVend), "utf8", function(err) {
+        /*
+        checa se o arquivo existe antes de salvar.
+        Se já existir, inclui um diferenciador no fim do arquivo
+        */
+        var i = 2;
+        while (fs.existsSync(pastaJsonVend + nomeArquivo+".json")){
+            if (fs.existsSync(pastaJsonVend + nomeArquivo+"v"+i+".json")){
+                nomeArquivo = nomeArquivo.replace("v"+i,"v"+i+1);
+            } else {
+                nomeArquivo = nomeArquivo + "v"+i;
+            }
+            i++;
+        }
+
+        //extensão pra identificar
+        nomeArquivo += ".json";
+
+        console.log("gravando arquivo " + nomeArquivo + " em " + pastaJsonVend);
+        fs.writeFile(pastaJsonVend + nomeArquivo, JSON.stringify(jsonVend), "utf8", function(err) {
             if (err) {
                return console.error(err);
             }
         });
+
         //use a linha abaixo para logar o conteúdo do post
         //console.log(JSON.parse(queryData));
         callback();
