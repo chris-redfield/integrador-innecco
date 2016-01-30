88d7bd10928d2b1afdd8d90f00e62e5c647108c0
avaloni <avaloni@users.noreply.github.com>
2016-01-30 09:56:23 -0200

    estrutura de pastas

---
+++ routes/vendas.js
@@ -0,0 +1,11 @@
+var Venda = require('../dao/venda');
+var express = require('express');
+var router = express.Router();
+
+router.route('/')
+  .get(function(req, res) {
+    // req = request
+    //res = response
+  })
+  .post(function(req, res) {
+  });
