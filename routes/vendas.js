8f52b5c09f7a79ba181ec3964cdbdfcc2cc62525
Christian Moryah <christianmoryah@gmail.com>
2016-01-31 22:58:13 -0200

    persistindo uma venda stub no banco

--- routes/vendas.js
+++ routes/vendas.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
-router.route('/')
-  .get(function(req, res) {
-    // req = request
-    //res = response
-    res.send('Hello World!');
-  })
-  .post(function(req, res) {
+  router.post('/', function(req, res, next) {
+
+    //  mostra o corpo inteiro do request
+    //  console.log(req.body);
+
+    var json = JSON.parse(req.body.payload);
+
+    //manda um item do json na resposta
+    //res.send(json.sale_date);
+    //manda um item do json pro log
+    //console.log(json.sale_date);
+
+
+      models.Venda.create({
+        id_vend: json.id,
+        data_emissao: json.sale_date,
+        //TODO verificar se a loja é bsbmemo ou 303 norte
+        cnpj_emitente: "14.271.394/0001-27"
+      });
+
+      res.send(json);
+
   });
 
 module.exports = router;
