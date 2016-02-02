f23e036136974c40a4bb93daf17b3f78d2e295d3
Christian Moryah <christianmoryah@gmail.com>
2016-02-02 12:30:01 -0200

    adicionando regra de negócio do CNPJ

--- routes/vendas.js
+++ routes/vendas.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
+const CNPJ303 = "14.271.394/0001-27";
+const IDREG303 = "02d59481-b67d-11e5-f667-a8d2d7daaf57";
+const IDREGAER = "02d59481-b656-11e5-f667-ad63e6b21a72";
+//TODO - Descobrir qual o CNPJ do aeroporto
+const CNPJAER = "";
 
   router.post('/', function(req, res, next) {
 
     //  mostra o corpo inteiro do request
     //  console.log(req.body);
 
+    var cnpj = "";
     var json = JSON.parse(req.body.payload);
 
     //manda um item do json na resposta
@@ -14,12 +20,21 @@ var models = require('../models');
     //manda um item do json pro log
     //console.log(json.sale_date);
 
+    //console.log(json.register_sale_payments[0].register_sale.register_id);
+
+    //Verifica qual o id do registrador, e associa ao CNPJ certo
+      if(json.register_sale_payments[0].register_sale.register_id == IDREG303){
+        cnpj = CNPJ303;
+      } else {
+        cnpj = CNPJAER;
+      }
 
       models.Venda.create({
         id_vend: json.id,
+        //TODO cologar o time zone nesse valor
         data_emissao: json.sale_date,
-        //TODO verificar se a loja é bsbmemo ou 303 norte
-        cnpj_emitente: "14.271.394/0001-27"
+        cnpj_emitente: cnpj,
+
       });
 
       res.send(json);
